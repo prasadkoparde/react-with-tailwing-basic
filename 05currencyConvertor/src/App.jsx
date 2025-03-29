bb24043481d61a22f912c4f870332579c9bdbbae
Prasad Koparde <70564012+prasadkoparde@users.noreply.github.com>
2025-03-30 00:05:45 +0530

    application_setup
    just started with fifth app with setup

---
+++ 05currencyConvertor/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      
+      <p className="bg-red-500 text-white p-4">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
